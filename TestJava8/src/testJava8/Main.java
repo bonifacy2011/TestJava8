ebcca3320d5ae1c0555cc72ee6684e17b85f8ef5
vktor <vktor@example.ru>
2016-12-14 16:43:02 +0300

    Commit 14/12/2016

---
+++ TestJava8/src/testJava8/Main.java
@@ -0,0 +1,39 @@
+package testJava8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello java 8");
+
+		List<String> lst = new ArrayList<String>();
+		lst.add("list1");
+		lst.add("list2");
+
+		lst.forEach(s -> System.out.println(s));
+
+		System.out.println();
+
+		lst.removeIf(s -> s == "list1");
+		lst.forEach(s -> System.out.println(s));
+
+		System.out.println();
+
+		lst.add("list1");
+		lst.add("list2");
+		lst.replaceAll(s -> s.toUpperCase());
+		lst.forEach(s -> System.out.println(s));
+		lst.replaceAll(String::toLowerCase);
+		lst.forEach(s -> System.out.println(s));
+
+		System.out.println();
+
+		lst.sort((x, y) -> x.length() - y.length());// сортировка строк по
+													// длинне
+		lst.forEach(s -> System.out.println(s));
+
+	}
+}
